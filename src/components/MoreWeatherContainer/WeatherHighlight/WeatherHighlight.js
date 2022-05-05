1243ca052eac47ecc49c4babc0b41fb65c262b06
erik-ng-3006 <erikng3006@gmail.com>
2022-05-05 22:40:43 +0200

    add weather highlight data

--- src/components/MoreWeatherContainer/WeatherHighlight/WeatherHighlight.js
+++ src/components/MoreWeatherContainer/WeatherHighlight/WeatherHighlight.js
@@ -1,8 +1,15 @@
 import React from 'react';
 import classes from './WeatherHighlight.module.css';
 import Card from '../../UI/Card';
+import { useSelector } from 'react-redux';
 
 const WeatherHighlight = () => {
+	const weatherData = useSelector((state) => state.weather.data);
+	const transformedWeatherData = weatherData[0] || {};
+	const style = {
+		width: `${transformedWeatherData.humidity}%`,
+	};
+
 	return (
 		<div className={classes.highlight}>
 			<h2>Today’s Highlights </h2>
@@ -10,27 +17,38 @@ const WeatherHighlight = () => {
 				<Card className={classes.card}>
 					<h3>Wind status</h3>
 					<p>
-						7<span>mph</span>
+						{Math.round(transformedWeatherData['wind_speed'])}
+						<span>mph</span>
 					</p>
-					<div>WSW</div>
+					<div>
+						{transformedWeatherData['wind_direction_compass']}
+					</div>
 				</Card>
 				<Card className={classes.card}>
 					<h3>Humidity</h3>
 					<p>
-						84<span>%</span>
+						{transformedWeatherData.humidity}
+						<span>%</span>
 					</p>
-					<div>....</div>
+					<div className={classes.flex}>
+						<div className={classes['humidity-bar']}>
+							<span style={style}></span>
+						</div>
+						<div>%</div>
+					</div>
 				</Card>
 				<Card className={classes.card}>
 					<h3>Visibility</h3>
 					<p>
-						6,4 <span>miles</span>
+						{Math.round(transformedWeatherData.visibility)}{' '}
+						<span>miles</span>
 					</p>
 				</Card>
 				<Card className={classes.card}>
 					<h3>Air Pressure</h3>
 					<p>
-						998 <span>mb</span>{' '}
+						{Math.round(transformedWeatherData['air_pressure'])}{' '}
+						<span>mb</span>{' '}
 					</p>
 				</Card>
 			</div>
