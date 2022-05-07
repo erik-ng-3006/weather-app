1ab68681e78eb77425e8228012af81bdded96ddd
erik-ng-3006 <erikng3006@gmail.com>
2022-05-07 17:38:41 +0200

    fix api

--- src/store/locationSlice.js
+++ src/store/locationSlice.js
@@ -1,5 +1,4 @@
 import { createSlice } from '@reduxjs/toolkit';
-import { uiActions } from './uiSlice';
 
 const initialState = {
 	locations: [],
@@ -19,31 +18,8 @@ export const locationSlice = createSlice({
 export const searchLocations = (inputValue) => {
 	return async (dispatch) => {
 		const getLocation = async () => {
-			(function () {
-				var cors_api_host = 'cors-anywhere.herokuapp.com';
-				var cors_api_url = 'https://' + cors_api_host + '/';
-				var slice = [].slice;
-				var origin =
-					window.location.protocol + '//' + window.location.host;
-				var open = XMLHttpRequest.prototype.open;
-				XMLHttpRequest.prototype.open = function () {
-					var args = slice.call(arguments);
-
-					// eslint-disable-next-line
-					var targetOrigin = /^https?:\/\/([^\/]+)/i.exec(args[1]);
-					if (
-						targetOrigin &&
-						targetOrigin[0].toLowerCase() !== origin &&
-						targetOrigin[1] !== cors_api_host
-					) {
-						args[1] = cors_api_url + args[1];
-					}
-					return open.apply(this, args);
-				};
-			})();
-
 			const response = await fetch(
-				`https://cors-anywhere.herokuapp.com/https://www.metaweather.com/api//api/location/search/?query=${inputValue}`
+				`https://mycorsproxy-crossdomainyz.herokuapp.com/https://www.metaweather.com/api//api/location/search/?query=${inputValue}`
 			);
 			if (!response.ok) {
 				throw new Error('Failed to get locations data');
@@ -59,7 +35,6 @@ export const searchLocations = (inputValue) => {
 			const transformedLocationData = locationsData.map((location) => {
 				return { name: location.title, id: location.woeid.toString() };
 			});
-			console.log(transformedLocationData);
 			dispatch(
 				locationSlice.actions.setLocations(
 					transformedLocationData || []
--- src/store/weatherSlice.js
+++ src/store/weatherSlice.js
@@ -22,32 +22,10 @@ export const weatherSlice = createSlice({
 export const fetchWeatherData = (locationId = '796597') => {
 	return async (dispatch) => {
 		dispatch(uiActions.setIsLoading(true));
+
 		const getData = async () => {
-			(function () {
-				var cors_api_host = 'cors-anywhere.herokuapp.com';
-				var cors_api_url = 'https://' + cors_api_host + '/';
-				var slice = [].slice;
-				var origin =
-					window.location.protocol + '//' + window.location.host;
-				var open = XMLHttpRequest.prototype.open;
-				XMLHttpRequest.prototype.open = function () {
-					var args = slice.call(arguments);
-
-					// eslint-disable-next-line
-					var targetOrigin = /^https?:\/\/([^\/]+)/i.exec(args[1]);
-					if (
-						targetOrigin &&
-						targetOrigin[0].toLowerCase() !== origin &&
-						targetOrigin[1] !== cors_api_host
-					) {
-						args[1] = cors_api_url + args[1];
-					}
-					return open.apply(this, args);
-				};
-			})();
-
 			const response = await fetch(
-				`https://cors-anywhere.herokuapp.com/https://www.metaweather.com/api/location/${locationId}`
+				`https://mycorsproxy-crossdomainyz.herokuapp.com/https://www.metaweather.com/api/location/${locationId}`
 			);
 
 			if (!response.ok) {
@@ -62,7 +40,6 @@ export const fetchWeatherData = (locationId = '796597') => {
 
 		try {
 			const weatherData = await getData();
-			console.log(weatherData);
 			const transformedWeatherData = weatherData['consolidated_weather'];
 			dispatch(
 				weatherActions.setWeatherData(transformedWeatherData || [])
